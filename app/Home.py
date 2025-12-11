869835fe615d7caef9ffe481c6bec68afa69826e
Mahbubalhassanofficial <90603087+Mahbubalhassanofficial@users.noreply.github.com>
2025-12-11 16:09:38 +0700

    Update Home.py

--- app/Home.py
+++ app/Home.py
@@ -11,7 +11,7 @@ from core.config import (
     PathConfig,
 )
 from core.generator import generate_dataset
-from app._branding import render_app_header, render_app_footer
+from app.branding import render_app_header, render_app_footer
 
 
 # ------------------------------------------------

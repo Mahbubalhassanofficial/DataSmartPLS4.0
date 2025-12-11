1e3a3b3ed6c40ad455380f4b7591ec426ded04ab
Mahbubalhassanofficial <90603087+Mahbubalhassanofficial@users.noreply.github.com>
2025-12-11 16:15:28 +0700

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
-st.set_page_config(page_title="DataSmartPLS4.0", layout="wide")
-st.switch_page("app/Home.py")
+
+st.set_page_config(
+    page_title="DataSmartPLS4.0",
+    layout="wide",
+)
+
+st.write("# Welcome to DataSmartPLS4.0")
+
+st.info("Use the sidebar to navigate between pages.")

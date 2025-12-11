20d09f6b93dd75d08043514dc58d5e0115202175
Mahbubalhassanofficial <90603087+Mahbubalhassanofficial@users.noreply.github.com>
2025-12-11 17:12:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,22 @@
 import streamlit as st
-from app.Home import main as home_page
+from app.Home import run as home
+from app.Structural import run as structural
+from app.Measurement import run as measurement
+from app.Bias import run as bias
+from app.Diagnostics import run as diagnostics
 
-home_page()
+PAGE = st.sidebar.selectbox(
+    "Navigation",
+    ["Home", "Structural Model", "Measurement Model", "Bias Simulation", "Diagnostics"]
+)
+
+if PAGE == "Home":
+    home()
+elif PAGE == "Structural Model":
+    structural()
+elif PAGE == "Measurement Model":
+    measurement()
+elif PAGE == "Bias Simulation":
+    bias()
+elif PAGE == "Diagnostics":
+    diagnostics()
